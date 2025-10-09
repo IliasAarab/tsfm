fba037716da9c8705803547a458c6ef551d9dd2c
Aarab, Ilias <ilias.aarab@ecb.europa.eu>
2025-10-09 19:45:50 +0200

    chore: update .gitignore and __init__.py for improved file management

--- src/tsfm/__init__.py
+++ src/tsfm/__init__.py
@@ -5,6 +5,7 @@ from tsfm.data import generator
 from tsfm.models.armodel import ARModel
 from tsfm.models.base import Model
 from tsfm.models.moirai import Moirai
-from tsfm.vis import plot_preds
 
 plt.style.use(["science", "no-latex", "notebook"])
+
+__all__ = ["ARModel", "Model", "Moirai", "generator"]
